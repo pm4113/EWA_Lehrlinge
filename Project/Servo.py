0eb2379500bd8705edfc28b465e99009604894d4
pm4113 <pm4113@mci4me.at>
2017-04-16 10:58:25 +0200

    Eingabe ohne Rückgabewert

--- Project/Servo.py
+++ Project/Servo.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from get_char import _Getch
+from pdb import set_trace as bp
+#os.system("raspivid -o - -t 0 -n -vf -hf -w 1280 -h 720 -fps 25 -g 100 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/,mux=ts}' :demux=h264") 
 
-os.system("raspivid -o - -t 0 -n -vf -hf -w 1280 -h 720 -fps 25 -g 100 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/,mux=ts}' :demux=h264")
 
 servoPIN_1 = 27
 EnablePIN_1 = 5
@@ -18,44 +20,50 @@ GPIO.output(EnablePIN_2, 1)
 
 p = GPIO.PWM(servoPIN_1, 50) # GPIO 17 als PWM mit 50Hz
 q = GPIO.PWM(servoPIN_2, 50)
-p.start(2.5) # Initialisierung
-q.start(2.5)
+
+cycle_channel = 0
+p.start(cycle_channel) # Initialisierung
+q.start(cycle_channel)
+
+getch = _Getch() 	# call class _Getch() from import file get_char
+
+print "Insert a character: "
+
+
+def DC_up():
+	DC_up.cycle_channel += 0.002
+#    q.ChangeDutyCycle(2.5)
+#    time.sleep(0.5)
+#	time.sleep(0.05)
+	return DC_up.cycle_channel
+
+def DC_down():
+	DC_down.cycle_channel -= 0.002
+	if DC_down.cycle_channel <=0:
+		DC_down.cycle_channel = 0
+	return DC_down.cycle_channel
+
+DC_up.cycle_channel = 0
+DC_down.cycle_channel = 0
+
+cycle = 1
 try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    q.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+	while True:
+		direction = getch()
+		bp()	
+		if direction == 'q':
+			print direction
+			cycle = DC_up()
+		elif direction == 'e':
+			print direction
+			cycle = DC_down()
+		else:
+			break
+     		
+     		p.ChangeDutyCycle(cycle)
+    	 	q.ChangeDutyCycle(cycle)
 
 except KeyboardInterrupt:
-  p.stop()
-  q.stop()
-  GPIO.cleanup()
+  	p.stop()
+  	q.stop()
+  	GPIO.cleanup()
