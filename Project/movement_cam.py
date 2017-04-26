c42bf06a5fcf68c22d9c7418dd488ebc1d04230d
pm4113 <pm4113@mci4me.at>
2017-04-26 15:35:35 +0200

    Es wurde eine neue Classe definiert welche über einen eigenen Thread läuft um im notfall eine selbst generiertes PWM Signal zu verwenden

--- Project/movement_cam.py
+++ Project/movement_cam.py
@@ -1,27 +1,57 @@
+import sys
+import time
+import threading
+from definition import *
+
+global PWM_time
+
+class TimerClass(threading.Thread):
+
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.event = threading.Event()
+
+	def run(self):
+		while not self.event.is_set():
+			print(time.time())
+			self.event.wait( PWM_time )
+
+	def stop(self):
+		self.event.set()
+
+tmr = TimerClass()
+
+def starttime():
+	tmr.start()
+
+def stoptime():
+	tmr.stop()
+
+
 def azimut_up(dc_azimut):
-        dc_azimut += 0.2
+        dc_azimut += 1
         if dc_azimut >= 30:
                 dc_azimut = 30
-#        print "DC_UP: " + str(dc_azimut)
+        print "DC_UP_AZI: " + str(dc_azimut)
         return dc_azimut
 
 def azimut_down(dc_azimut):
-        dc_azimut -= 0.2
+        dc_azimut -= 1
         if dc_azimut <= 0:
                 dc_azimut = 0
-#        print "DC_DOWN: " + str(dc_azimut)
+        print "DC_DOWN_AZI: " + str(dc_azimut)
         return dc_azimut
 
 def elevation_up(dc_elevation):
-        dc_elevation += 0.2
+        dc_elevation += 1
         if dc_elevation >= 30:
                 dc_elevation = 30
-#        print "DC_UP: " + str(dc_elevation)
+        print "DC_UP_ELEV: " + str(dc_elevation)
         return dc_elevation
 
 def elevation_down(dc_elevation):
-        dc_elevation -= 0.2
+        dc_elevation -= 1
         if dc_elevation <= 0:
                 dc_elevation = 0
-#        print "DC_DOWN: " + str(dc_elevation)
+        print "DC_DOWN_ELEV: " + str(dc_elevation)
         return dc_elevation
